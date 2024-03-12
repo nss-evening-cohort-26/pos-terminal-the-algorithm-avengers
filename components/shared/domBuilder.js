798cf3ec6f6a99c914ae2f9bc27d71c48c9f5d2d
Jesse French <frejes32@gmail.com>
2024-03-12 18:12:11 -0500

    Added requested changes per PR review

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -4,7 +4,9 @@ const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
   <div id="main-container">
-    
+    <div id="store"></div>
+    <div id="view"></div>
+    <div id="form-container"></div>
   </div>`;
   renderToDom('#app', domString);
 };
--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,5 +1,7 @@
 const clearDom = () => {
-
+  document.querySelector('#store').innerHTML = '';
+  document.querySelector('#view').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = '';
 };
 
 export default clearDom;
