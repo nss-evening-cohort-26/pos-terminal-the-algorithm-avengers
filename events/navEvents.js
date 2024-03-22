cb0a74cad9c4380984ff96fbbfd601a9f3ed0821
Jesse French <frejes32@gmail.com>
2024-03-21 20:18:25 -0500

    Added search functionality

--- events/navEvents.js
+++ events/navEvents.js
@@ -1,6 +1,9 @@
+import { searchOrders } from '../api/mergedData';
 import { getOrders } from '../api/orderData';
 import addOrderForm from '../components/forms/newOrderForm';
 import { showOrders } from '../pages/orders';
+import clearDom from '../utils/clearDom';
+import renderToDom from '../utils/renderToDom';
 
 const navEvents = (uid) => {
   document.querySelector('#navigation').addEventListener('click', (e) => {
@@ -10,6 +13,24 @@ const navEvents = (uid) => {
 
     if (e.target.id.includes('create_order_btn')) {
       addOrderForm();
+    }
+  });
+
+  const searchBar = document.querySelector('#search');
+  searchBar.addEventListener('keyup', (e) => {
+    const searchValue = searchBar.value.toLowerCase();
+
+    if (e.keyCode === 13) {
+      searchBar.value = '';
+      searchOrders(uid, searchValue).then(({ orders }) => {
+        if (orders.length > 0) {
+          showOrders(orders, uid);
+        } else {
+          clearDom();
+          const domString = '<h1>No results.</h1>';
+          renderToDom('#store', domString);
+        }
+      });
     }
   });
 };
